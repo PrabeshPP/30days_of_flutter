1c1d763a8151d27e3d46e9780d9f981a778b5a5f
Prabesh Bista <prabesh.bista1000@gmail.com>
2021-07-15 18:58:09 +0545

    day4completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/screens/homepage.dart';
 import 'package:flutter_application_1/screens/loginpage.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 
 
@@ -15,8 +16,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,//bascially remove the debug from the top right corner
+      
       themeMode:ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.red),
+      theme: ThemeData(primarySwatch: Colors.red,
+      fontFamily: GoogleFonts.lato().fontFamily),
       darkTheme: ThemeData(brightness: Brightness.dark),
       routes: {
         "/":(context)=>Login(),
--- lib/screens/loginpage.dart
+++ lib/screens/loginpage.dart
@@ -5,10 +5,36 @@ class  Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(child: Text(
-        "Login",
-        style: TextStyle(color: Colors.red),
-      ),),
+      color: Colors.white,
+     
+     child: Column(
+       children: [
+         Image.asset("assets/images/log.png",fit: BoxFit.cover),
+         SizedBox(
+           height: 50,
+         ),
+         Text("Welcome",style: TextStyle(fontSize: 20
+         ,color: Colors.deepPurple,fontWeight: FontWeight.bold
+
+         ),
+         ),
+         TextFormField(
+           decoration: InputDecoration(
+             hintText: "username",
+             labelText: "UserName"
+              
+
+           ),
+         ),
+
+        TextFormField(
+          decoration: InputDecoration(
+            hintText: "password",
+            labelText: "Password"
+          ),
+        )
+       ],
+     ),
       
     );
   }
