b1e5a1d6e7903c5b16e18b64036bd98a9b4cb516
Prabesh Bista <prabesh.bista1000@gmail.com>
2021-07-16 10:34:57 +0545

    worked on validation

--- lib/screens/loginpage.dart
+++ lib/screens/loginpage.dart
@@ -10,105 +10,131 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   String name = " ";
   bool changed = false;
+  final _formkey = GlobalKey<FormState>();
+
+  moveToHome(BuildContext context) async {
+    if (_formkey.currentState!.validate()) {
+      setState(() {
+        changed = true;
+      });
+      await Future.delayed(Duration(seconds: 1));
+      await Navigator.pushNamed(context, MyRoutes.homeRoute);
+      setState(() {
+        changed = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
       child: SingleChildScrollView(
-        child: Column(
-          children: [
-            Image.asset("assets/images/log.png", fit: BoxFit.contain),
-            SizedBox(
-              height: 50,
-            ),
-            Text(
-              "Welcome ${name != "" ? name + "!" : name}",
-              style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.deepPurple,
-                  fontWeight: FontWeight.bold),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
-              child: Column(
-                children: [
-                  TextFormField(
-                    cursorColor: Colors.lightBlue,
-                    decoration: InputDecoration(
-                      labelText: "UserName",
-                      hintText: "Enter Username",
-                      labelStyle: TextStyle(
-                          color: Colors.black, fontWeight: FontWeight.w500),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.deepPurple,
-                        ),
-                      ),
-                      border: OutlineInputBorder(borderSide: BorderSide()),
-                    ),
-                    onChanged: (value) {
-                      name = value;
-                      setState(() {});
-                    },
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                        labelText: "Password",
-                        hintText: "Enter Password",
-                        labelStyle: TextStyle(
-                            color: Colors.black,
-                            fontSize: 15,
-                            fontWeight: FontWeight.w500),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.deepPurple),
-                        ),
-                        border: OutlineInputBorder(borderSide: BorderSide())),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  InkWell(
-                    onTap: () async {
-                      setState(() {
-                        changed = true;
-                      });
-                      await Future.delayed(Duration(seconds: 1));
-                      Navigator.pushNamed(context, MyRoutes.homeRoute);
-                    },
-                    child: AnimatedContainer(
-                      duration: Duration(seconds: 1),
-                      height: 50,
-                      width: changed ? 50 : 120,
-                      alignment: Alignment.center,
-                      child: changed
-                          ? Icon(
-                              Icons.done,
-                              color: Colors.pink,
-                              size: 30,
-                            )
-                          : Text(
-                              "Login",
-                              style: TextStyle(
-                                color: Colors.blue,
-                                fontSize: 20,
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-                      decoration: BoxDecoration(
-                          color: Colors.yellow,
-                          borderRadius:
-                              BorderRadius.circular(changed ? 50 : 8)),
-                    ),
-                  )
-                ],
+        child: Form(
+          key: _formkey,
+          child: Column(
+            children: [
+              Image.asset("assets/images/log.png", fit: BoxFit.contain),
+              SizedBox(
+                height: 30,
               ),
-            )
-          ],
+              Text(
+                "Welcome ${name != "" ? name + "!" : name}",
+                style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.deepPurple,
+                    fontWeight: FontWeight.bold),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      cursorColor: Colors.lightBlue,
+                      decoration: InputDecoration(
+                        labelText: "UserName",
+                        hintText: "Enter Username",
+                        labelStyle: TextStyle(
+                            color: Colors.black, fontWeight: FontWeight.w500),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.deepPurple,
+                          ),
+                        ),
+                        border: OutlineInputBorder(borderSide: BorderSide()),
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "username cannot be empty";
+                        }
+
+                        return null;
+                      },
+                      onChanged: (value) {
+                        name = value;
+                        setState(() {});
+                      },
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          labelText: "Password",
+                          hintText: "Enter Password",
+                          labelStyle: TextStyle(
+                              color: Colors.black,
+                              fontSize: 15,
+                              fontWeight: FontWeight.w500),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.deepPurple),
+                          ),
+                          border: OutlineInputBorder(borderSide: BorderSide())),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "password cannot be empty";
+                        } else if (value.length < 6) {
+                          return "password length should be at least six";
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Material(
+                        color: Colors.yellow,
+                        borderRadius: BorderRadius.circular(changed ? 50 : 8),
+                        child: InkWell(
+                          onTap: () => moveToHome(context),
+                          child: AnimatedContainer(
+                            duration: Duration(seconds: 1),
+                            height: 50,
+                            width: changed ? 50 : 120,
+                            alignment: Alignment.center,
+                            child: changed
+                                ? Icon(
+                                    Icons.done,
+                                    color: Colors.pink,
+                                    size: 30,
+                                  )
+                                : Text(
+                                    "Login",
+                                    style: TextStyle(
+                                      color: Colors.blue,
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                          ),
+                        )),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
