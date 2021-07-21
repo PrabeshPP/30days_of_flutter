c4b0f9a0adb8e2ae33a7ecf01333b0f57555692d
Prabesh Bista <prabesh.bista1000@gmail.com>
2021-07-21 19:55:46 +0545

    worked on cart page

--- lib/screens/cartpage.dart
+++ lib/screens/cartpage.dart
@@ -3,21 +3,75 @@ import 'package:flutter/material.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 class Cartpage extends StatelessWidget {
-  const Cartpage({ Key? key }) : super(key: key);
+  const Cartpage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
       appBar: AppBar(
-        backgroundColor:context.canvasColor ,
+        backgroundColor: context.canvasColor,
         title: "My Cart".text.xl.bold.make(),
       ),
-      body:Container(
-        child: Center(child: "Comming Soon!".text.bold.xl.make()),
-      ) ,
-      
-    
+      body: Column(
+        children: [
+          _CartList().p32().expand(),
+          Divider(),
+          _CartTotal(),
+        ],
+      ),
+    );
+  }
+}
+
+class _CartTotal extends StatelessWidget {
+  const _CartTotal({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 50,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          "All".text.xl.make().pOnly(left: 20),
+          ElevatedButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all(StadiumBorder()),
+                    backgroundColor: MaterialStateProperty.all(Colors.orange),
+                  ),
+                  child: "Buy".text.make())
+              .pOnly(right: 30)
+              .wh(100, 40)
+        ],
+      ),
+    );
+  }
+}
+
+class _CartList extends StatefulWidget {
+  const _CartList({Key? key}) : super(key: key);
+
+  @override
+  __CartListState createState() => __CartListState();
+}
+
+class __CartListState extends State<_CartList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 5,
+      itemBuilder: (context, index) {
+        return ListTile(
+          leading: Icon(
+            Icons.done,
+            size: 30,
+          ),
+          trailing: IconButton(onPressed: (){}, icon: Icon(Icons.remove_circle_outline,
+          size: 30,)),
+          title: "Item1".text.make(),
+        );
+      },
     );
   }
 }
